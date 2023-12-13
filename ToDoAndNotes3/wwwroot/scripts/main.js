e3605e50d019e006a518a86b804bf9070288198c
SerhiiShtepa <serhii.shtepa.@gmail.com>
2023-12-13 12:55:37 +0200

    Implement the behavior of the content on the mobile screen when the sidebar is open.

--- ToDoAndNotes3/wwwroot/scripts/main.js
+++ ToDoAndNotes3/wwwroot/scripts/main.js
@@ -1,5 +1,28 @@
-$(function () {
-   $('#burger-menu-toggle').on('click', function () {
-      $('#sidebar').toggleClass('active');
-   });
+document.addEventListener('DOMContentLoaded', function () {
+    var burgerMenuToggle = document.getElementById('burger-menu-toggle');
+    var sidebar = document.getElementById('sidebar');
+
+    burgerMenuToggle.addEventListener('click', function () {
+        sidebar.classList.toggle('sidebar-hide');
+        checkWindowSize();
+    });
 });
+
+// Hide main if (is on mobile screen) + (sidebar is shown)
+function checkWindowSize() {
+    const aside = document.getElementById('sidebar');
+    const main = document.getElementById('main');
+
+    if (window.innerWidth <= 768 && !aside.classList.contains('sidebar-hide')) {
+        main.style.visibility = 'hidden';
+        main.style.opacity = 0;
+    }
+    else {
+        main.style.visibility = 'visible';
+        main.style.opacity = 1;
+
+    }
+}
+
+window.addEventListener('load', checkWindowSize);
+window.addEventListener('resize', checkWindowSize);
